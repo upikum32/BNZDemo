1e6db0f941c6ef30b49824075389f64b457c08ad
Upinder Kumar <upenderbharti@.com>
2021-11-25 07:47:30 +1300

    Add project files.

---
+++ BNZDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BNZDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BNZDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94165afd-ee03-4452-8fe7-a029529fa8bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BNZDemo/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+namespace BNZDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ClickMenu()
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://www.demo.bnz.co.nz/client/");
+            string linkMenu = "//button[contains(@class,'Button Button--transparent js-main-menu-btn MenuButton')]//button[contains(@class,'Button Button--transparent js-main-menu-btn MenuButton')]";
+            Driver.FindElement(By.XPath(linkMenu)).Click();
+        }
+        [TestMethod]
+        public void ClickPayee()
+        {
+            IWebDriver Driver = new ChromeDriver();
+            string linkPayee = "//span[contains(@class,'Language__container') and text()='Payees']/ancestor::a";
+          
+            Driver.FindElement(By.XPath(linkPayee)).Click();
+            Driver.Quit();
+        }
+    }
+}
+
+
+
+
+     
